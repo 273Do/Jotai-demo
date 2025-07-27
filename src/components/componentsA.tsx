ab926dc8b08f0f93f6619b1a71ef982065b99d66
273* <keita.y310@icloud.com>
2025-07-27 22:49:04 +0900

    テスト用コンポーネントの作成

---
+++ src/components/componentsA.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ComponentsA = () => {
+  return <div>ComponentsA</div>;
+};
+
+export default ComponentsA;
---
+++ src/components/componentsB.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ComponentsB = () => {
+  return <div>ComponentsB</div>;
+};
+
+export default ComponentsB;
