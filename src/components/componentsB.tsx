e9e8f476ead6bd18393c70df925a7c3d2ea7def4
273* <keita.y310@icloud.com>
2025-07-28 00:21:33 +0900

    ブローバル管理の実装

--- src/components/componentsB.tsx
+++ src/components/componentsB.tsx
@@ -1,7 +1,25 @@
+import { useSetAtom } from "jotai";
+import { todoAtom } from "../atoms/todAtm";
+
 const ComponentsB = () => {
+  const setTodos = useSetAtom(todoAtom);
   return (
     <div>
       <p>ADD TODO</p>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const newTodo = {
+            id: Date.now(),
+            text: e.currentTarget.todo.value,
+            completed: false,
+          };
+          setTodos((prevTodos) => [...prevTodos, newTodo]);
+        }}
+      >
+        <input type="text" name="todo" />
+        <button type="submit">Add</button>
+      </form>
     </div>
   );
 };
--- src/components/componentsC.tsx
+++ src/components/componentsC.tsx
@@ -1,7 +1,19 @@
+import { useAtomValue } from "jotai";
+import { todoAtom } from "../atoms/todAtm";
+
 const ComponentsC = () => {
+  const todos = useAtomValue(todoAtom);
   return (
     <div>
       <p>Completed TODOs</p>
+      {todos
+        .filter((todo) => todo.completed)
+        .map((todo) => (
+          <div key={todo.id} style={{ display: "flex", alignItems: "center" }}>
+            <input type="checkbox" checked={todo.completed} />
+            <p>{todo.text}</p>
+          </div>
+        ))}
     </div>
   );
 };
